902ddd60f66caeca1f0949d7e05d90385ef2cc12
Sarkaut Salih <111055810+Sarkaaut@users.noreply.github.com>
2023-05-01 21:29:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ app = Client(
 
 
 if __name__ == "__main__":
-    print("Starting the String Generator Bot...")
+    print("دەستپێکردنی بۆتی مۆلیدەی سترینگ...")
     try:
         app.start()
     except (ApiIdInvalid, ApiIdPublishedFlood):
@@ -32,4 +32,4 @@ if __name__ == "__main__":
     print(f"@{uname} started successfully !")
     idle()
     app.stop()
-    print("Bot stopped. Bye !")
+    print("بۆت وەستا بای!")
